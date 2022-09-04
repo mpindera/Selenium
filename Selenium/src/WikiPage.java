0d857188b54c1bf2ae37c0a9933b84fb3380c20a
mpindera <107795584+mpindera@users.noreply.github.com>
2022-09-04 13:57:44 +0200

    Update WikiPage.java

--- Selenium/src/WikiPage.java
+++ Selenium/src/WikiPage.java
@@ -1,14 +1,12 @@
-import java.util.HashMap;
-
 public class WikiPage {
     private final String url;
     private final String title;
-    private final String content;
+    private final StringBuilder content;
 
     public WikiPage(String url, String title, StringBuilder content) {
         this.url = url;
         this.title = title;
-        this.content = String.valueOf(content);
+        this.content = content;
     }
 
     public String toString() {
